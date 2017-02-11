2c39b8c81ea2e02f928b542acaef924ba148126a
Tom Walker <tpwalke2@gmail.com>
2017-02-11 06:04:47 -0500

    Implementation plan update, adding initial classes

---
+++ KualiTest/ElevatorSim/ElevatorSim/Elevator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSim
+{
+    public class Elevator: IElevatorObserver
+    {
+        enum ElevatorStatus
+        {
+            StoppedOpen,
+            StoppedClosed,
+            Moving,
+            Maintenance
+        }
+
+        private int _maxFloor;
+        private ElevatorStatus _currentStatus;
+        private int _currentFloor;
+        private IList<IElevatorObserver> _observers;
+
+        public Elevator(string uid, int maxFloor)
+        {
+            this.UID = uid;
+            _maxFloor = maxFloor;
+            _currentFloor = 1;
+            _currentStatus = ElevatorStatus.StoppedClosed;
+            _observers = new List<IElevatorObserver>();
+        }
+
+        public string UID { get; }
+
+        public void AddObserver(IElevatorObserver observer)
+        {
+            if (!_observers.Contains(observer)) { _observers.Add(observer); }
+        }
+    }
+}
---
+++ KualiTest/ElevatorSim/ElevatorSim/ElevatorBank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSim
+{
+    public class ElevatorBank : IElevatorObserver
+    {
+        private IList<Elevator> _elevators;
+
+        public ElevatorBank()
+        {
+            _elevators = new List<Elevator>();
+        }
+
+        public void initialize(int numElevators, int numFloors)
+        {
+            for (int i = 1; i <= numElevators; i++)
+            {
+                var newElevator = new Elevator(Guid.NewGuid().ToString(), numFloors);
+                newElevator.AddObserver(this);
+                _elevators.Add(newElevator);
+            }
+        }
+    }
+}
